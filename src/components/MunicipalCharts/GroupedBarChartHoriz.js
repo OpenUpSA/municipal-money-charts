e3180d6b617a43c80836d4d0795866056b2e4f77
michaelglenister <mickeyjhg@gmail.com>
2023-05-02 15:59:02 +0200

    Add year label

--- src/components/MunicipalCharts/GroupedBarChartHoriz.js
+++ src/components/MunicipalCharts/GroupedBarChartHoriz.js
@@ -61,6 +61,7 @@ export default class GroupedBarChartHoriz extends MunicipalChart {
               .selectAll('.item-bar')
               .data(Object.values(d.data))
               .join(enter => enter.append('div').style('width', '0%'))
+              .text(d => d.financial_year)
               .attr('class', d => `bar-main`)
               .classed('item-bar', true)
               .attr('data-tooltip', d => d.amount === null ? "Not available" : format(d.amount))
